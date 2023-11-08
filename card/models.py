ceeaf75b05b9762390e23c3a0da1d644d346798d
Roland Kainbacher <kainbacher@users.noreply.github.com>
2023-11-08 14:45:20 +0100

    footer styling

--- card/models.py
+++ card/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin, Page
 from filer.fields.image import FilerImageField
